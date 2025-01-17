aac0fdceca569ca6f28541b0539085b267eea067
Sunil Poudel <suneelpdl90@gmail.com>
2025-01-17 12:32:06 +0545

    upto youtube accessibility xml

---
+++ app/src/main/java/com/sunil_poudel/accessibilityapplication/accessibility/YoutubeAccessibilityService.java
@@ -0,0 +1,16 @@
+package com.sunil_poudel.accessibilityapplication.accessibility;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class YoutubeAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
